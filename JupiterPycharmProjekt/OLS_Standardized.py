149dd4d5774e61cae765ba669442c4dbc6bbbdb0
NinaBra <nina.brandt@fs-students.de>
2025-07-22 19:10:43 -0400

    22.07.2025 19:10 Nina

--- JupiterPycharmProjekt/OLS_Standardized.py
+++ JupiterPycharmProjekt/OLS_Standardized.py
@@ -1,23 +1,33 @@
 from sklearn.linear_model import LinearRegression
 import pandas as pd
-from matplotlib import pyplot as plt
 from sklearn.metrics import r2_score
 from sklearn.model_selection import train_test_split
-import seaborn as sns
 from sklearn import preprocessing
 from sklearn.preprocessing import LabelEncoder
-import numpy as np
 
+###########
+# One-Hot #
+###########
 
 # Read original Data from CSV
 data = pd.read_csv('UsedCarSellingPrices.csv')
 print(data.head())
+
+data['Brand'] = data['Bame'].str.split().str[0]
+print(data)
+
+data = data.drop(columns=['name'])
 
 data_numeric = pd.get_dummies(data)
 print(data_numeric.head())
 
 nscaler = preprocessing.MinMaxScaler()
 data = nscaler.fit_transform(data_numeric)
+# data = (data_numeric - data_numeric.min()) / (data_numeric.max() - data_numeric.min())
+
+print("data before df")
+print(data.shape)
+print(data)
 data_df = pd.DataFrame(data, columns=data_numeric.columns)
 print(data_df.head())
 
@@ -35,14 +45,19 @@ lm.fit(X_train, Y_train)
 
 Y_train_pred = lm.predict(X_train)
 
-R2 = r2_score(Y_train, Y_train_pred)
+R2_OH = r2_score(Y_train, Y_train_pred)
 # Y_train_dev = sum((Y_train - Y_train_pred) ** 2)
 # R2 = 1 - (Y_train_dev / Y_train_meandev)
-print(R2)
+print(R2_OH)
 
 Y_test_pred = lm.predict(X_test)
-PseudoR2 = r2_score(Y_test, Y_test_pred)
+PseudoR2_OH = r2_score(Y_test, Y_test_pred)
 
 # Y_test_dev = sum((Y_test - Y_test_pred) ** 2)
 # PseudoR2 = 1 - (Y_test_dev / Y_train_meandev)
-print(PseudoR2)
+print(PseudoR2_OH)
+
+#################
+# Lable Encoded #
+#################
+
