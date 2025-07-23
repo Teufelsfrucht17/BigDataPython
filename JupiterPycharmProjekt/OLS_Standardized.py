79ca1fba43b068566c1b96db773a6875b2c28592
NinaBra <nina.brandt@fs-students.de>
2025-07-22 21:23:21 -0400

    22.07.2025 21:20 Nina

--- JupiterPycharmProjekt/OLS_Standardized.py
+++ JupiterPycharmProjekt/OLS_Standardized.py
@@ -5,9 +5,6 @@ from sklearn.model_selection import train_test_split
 from sklearn import preprocessing
 from sklearn.preprocessing import LabelEncoder
 
-###########
-# One-Hot #
-###########
 
 # Read original Data from CSV
 data = pd.read_csv('UsedCarSellingPrices.csv')
@@ -17,6 +14,55 @@ data['Brand'] = data['name'].str.split().str[0]
 #print(data)
 
 data = data.drop(columns=['name'])
+
+
+#################
+# Lable Encoded #
+#################
+
+data_LE = data.copy()
+categorical_columns = data_LE.select_dtypes(include=['object']).columns
+
+label_encoder = LabelEncoder()
+for column in categorical_columns:
+    data_LE[column] = label_encoder.fit_transform(data_LE[column])
+
+nscaler = preprocessing.MinMaxScaler()
+data_scaled_LE = nscaler.fit_transform(data_LE)
+
+data_df_LE = pd.DataFrame(data_scaled_LE, columns=data.columns)
+#print(data_df_LE.shape)
+#print(data_df_LE)
+
+X = data_df_LE.drop(columns=['selling_price']) # Feature
+Y = data_df_LE['selling_price'] # Variable
+
+X_train_LE, X_test_LE, Y_train_LE, Y_test_LE = train_test_split(X, Y, test_size=0.2, random_state=42)
+print(Y_train_LE.shape)
+print(Y_test_LE.shape)
+
+lm = LinearRegression()
+lm.fit(X_train_LE, Y_train_LE)
+
+Y_train_pred_LE = lm.predict(X_train_LE)
+
+R2_LE = r2_score(Y_train_LE, Y_train_pred_LE)
+# Y_train_dev = sum((Y_train - Y_train_pred) ** 2)
+# R2 = 1 - (Y_train_dev / Y_train_meandev)
+print("R2 LE: ")
+print(R2_LE)
+
+Y_test_pred_LE = lm.predict(X_test_LE)
+PseudoR2_LE = r2_score(Y_test_LE, Y_test_pred_LE)
+
+# Y_test_dev = sum((Y_test - Y_test_pred) ** 2)
+# PseudoR2 = 1 - (Y_test_dev / Y_train_meandev)
+print("PseudoR2 LE: ")
+print(PseudoR2_LE)
+
+###########
+# One-Hot #
+###########
 
 data_OH = pd.get_dummies(data)
 #print(data_OH.head())
@@ -57,3 +103,4 @@ PseudoR2_OH = r2_score(Y_test_OH, Y_test_pred_OH)
 # PseudoR2 = 1 - (Y_test_dev / Y_train_meandev)
 print("PseudoR2 OH: ")
 print(PseudoR2_OH)
+
