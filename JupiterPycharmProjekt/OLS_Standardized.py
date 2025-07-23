1b66e4db3e3d29eb83ac3ae5e23164f7f55cd491
NinaBra <nina.brandt@fs-students.de>
2025-07-22 21:18:15 -0400

    22.07.2025 19:10 Nina

--- JupiterPycharmProjekt/OLS_Standardized.py
+++ JupiterPycharmProjekt/OLS_Standardized.py
@@ -11,53 +11,50 @@ from sklearn.preprocessing import LabelEncoder
 
 # Read original Data from CSV
 data = pd.read_csv('UsedCarSellingPrices.csv')
-print(data.head())
+#print(data.head())
 
 data['Brand'] = data['name'].str.split().str[0]
-print(data)
+#print(data)
 
 data = data.drop(columns=['name'])
 
-data_numeric = pd.get_dummies(data)
-print(data_numeric.head())
+data_OH = pd.get_dummies(data)
+#print(data_OH.head())
 
 nscaler = preprocessing.MinMaxScaler()
-data = nscaler.fit_transform(data_numeric)
+data_scaled_OH = nscaler.fit_transform(data_OH)
 # data = (data_numeric - data_numeric.min()) / (data_numeric.max() - data_numeric.min())
 
-print("data before df")
-print(data.shape)
-print(data)
-data_df = pd.DataFrame(data, columns=data_numeric.columns)
-print(data_df.head())
+#print(data_scaled_OH.shape)
+#print(data_scaled_OH.shape)
+data_df_OH = pd.DataFrame(data_scaled_OH, columns=data_OH.columns)
+#print(data_df_OH.head())
 
-data_df.to_csv('DataNormed.csv', index=False)
+data_df_OH.to_csv('One_Hot_Data.csv', index=False)
 
-X = data_df.drop(columns=['selling_price']) # Feature
-Y = data_df['selling_price'] # Variable
+X = data_df_OH.drop(columns=['selling_price']) # Feature
+Y = data_df_OH['selling_price'] # Variable
 
-X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
-print(Y_train.shape)
-print(Y_test.shape)
+X_train_OH, X_test_OH, Y_train_OH, Y_test_OH = train_test_split(X, Y, test_size=0.2, random_state=42)
+print(Y_train_OH.shape)
+print(Y_test_OH.shape)
 
 lm = LinearRegression()
-lm.fit(X_train, Y_train)
+lm.fit(X_train_OH, Y_train_OH)
 
-Y_train_pred = lm.predict(X_train)
+Y_train_pred_OH = lm.predict(X_train_OH)
 
-R2_OH = r2_score(Y_train, Y_train_pred)
+R2_OH = r2_score(Y_train_OH, Y_train_pred_OH)
 # Y_train_dev = sum((Y_train - Y_train_pred) ** 2)
 # R2 = 1 - (Y_train_dev / Y_train_meandev)
+print("R2 OH: ")
 print(R2_OH)
 
-Y_test_pred = lm.predict(X_test)
-PseudoR2_OH = r2_score(Y_test, Y_test_pred)
+Y_test_pred_OH = lm.predict(X_test_OH)
+PseudoR2_OH = r2_score(Y_test_OH, Y_test_pred_OH)
 
 # Y_test_dev = sum((Y_test - Y_test_pred) ** 2)
 # PseudoR2 = 1 - (Y_test_dev / Y_train_meandev)
+print("PseudoR2 OH: ")
 print(PseudoR2_OH)
-
-#################
-# Lable Encoded #
-#################
 
