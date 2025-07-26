c68e6e5b45f5f0908b7f3cd8e6c19c960f8a6047
NinaBra <nina.brandt@fs-students.de>
2025-07-25 20:05:40 -0400

    24.07.2025 20:05 Nina

--- JupiterPycharmProjekt/Visualisation.py
+++ JupiterPycharmProjekt/Visualisation.py
@@ -1,13 +1,11 @@
 import pandas as pd
+import DataPrep
 from matplotlib import pyplot as plt
 from sklearn.model_selection import train_test_split
 import seaborn as sns
 from sklearn import preprocessing
 from sklearn.preprocessing import LabelEncoder
 import numpy as np
-
-# Read original Data from CSV
-
 
 #######################################
 # Visualisation before cleaining Data #
@@ -16,27 +14,21 @@ import numpy as np
 # All Data containes NO names but brands
 data = pd.read_csv('AllData.csv')
 
-# Boxplot with readable x-achsis
+'''
 plt.figure(figsize=(12, 6))
 sns.boxplot(data=data, orient='v', palette='Set2')
-plt.xticks(rotation=45)
-plt.title("Normed boxplot")
-plt.tight_layout()
+plt.show()
+'''
+
+# Visualisation of all data Lable encoded
+plt.figure(figsize=(12, 6))
+sns.boxplot(data=DataPrep.data_df_LE, orient='v', palette='Set2')
+plt.title("Boxplot - All Label Encoded Data")
 plt.show()
 
-data = pd.read_csv('Lable_Encoded_Data.csv')
-
-# Boxplot for only numerical data
+#DataFocus = DataPrep.data_df_LE[]
+# Visualisation of all data Lable encoded
+plt.figure(figsize=(12, 6))
 selected_cols = ['selling_price', 'km_driven', 'year']
-
-plt.figure(figsize=(8, 5))
-#sns.boxplot(data=data[selected_cols], orient='v', palette='Set3')
-sns.boxplot(data=data, orient='v', palette='Set2')
-plt.title("Normed boxplot for numerical data only")
-plt.tight_layout()
-plt.show()
-
-# Pairplot to show correlation
-sns.pairplot(data[selected_cols])
-plt.suptitle("Pairplot for select charactaristics", y=1.02)
+sns.pairplot(data=DataPrep.data_df_LE[selected_cols], palette='Set3')
 plt.show()
