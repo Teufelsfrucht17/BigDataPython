d92e106872284d5c41ec671a9f2168f064f70307
NinaBra <nina.brandt@fs-students.de>
2025-07-25 23:44:26 -0400

    25.07.2025 23:45 Nina

--- JupiterPycharmProjekt/Visualisation.py
+++ JupiterPycharmProjekt/Visualisation.py
@@ -7,8 +7,7 @@ from sklearn import preprocessing
 from sklearn.preprocessing import LabelEncoder
 import numpy as np
 
-# Load All Data containes NO names but brands
-data = pd.read_csv('Lable_Encoded_Data.csv')
+# define all numerical data from orininal file
 selected_cols = ['selling_price', 'km_driven', 'year']
 
 #######################################
@@ -32,23 +31,24 @@ sns.pairplot(data=DataPrep.data_df_LE_before_IQR[selected_cols], palette='Set3')
 plt.title("Pairplot - Original - All Label Encoded Data")
 plt.show()
 
+
 #######################################
 # Visualisation AFTER cleaining Data #
 #######################################
 
 # Visualisation of all data Lable encoded
 plt.figure(figsize=(12, 6))
-sns.boxplot(data=data, orient='v', palette='Set2')
+sns.boxplot(data=DataPrep.data_df_LE, orient='v', palette='Set2')
 plt.title("Boxplot - Cleaned - All Label Encoded Data")
 plt.show()
 
 # Visualisation of only nomerical values Lable encoded
 plt.figure(figsize=(12, 6))
-sns.boxplot(data=data[selected_cols], orient='v', palette='Set2')
+sns.boxplot(data=DataPrep.data_df_LE[selected_cols], orient='v', palette='Set2')
 plt.title("Boxplot - Cleaned - Numerical values Label Encoded Data")
 plt.show()
 
 # Visualisation of all data Lable encoded
-sns.pairplot(data=data[selected_cols], palette='Set3')
+sns.pairplot(data=DataPrep.data_df_LE[selected_cols], palette='Set3')
 plt.title("Pairplot - Cleaned - All Label Encoded Data")
 plt.show()
